236b242a561666f8ca77e84c07299cd38978127f
Rundll86 <2028673870@qq.com>
2025-07-13 23:06:19 +0800

    refactor(promiser): 重命名 WorkerPromiseController 为 AsyncWorker 并优化内部变量命名
    将 WorkerPromiseController 类重命名为更简洁的 AsyncWorker，同时将内部 resolve/reject 变量重命名为更清晰的 resolver/rejector。优化了代码可读性和一致性。

--- promiser/src/index.ts
+++ promiser/src/index.ts
@@ -5,29 +5,29 @@ declare const $func: PromiseExecutor<unknown>;
 export function createIIFE(func: Function) {
     return `(${func})()` as const;
 }
-export class WorkerPromiseController<T> {
+export class AsyncWorker<T> {
     runnerCode: string | null = null;
     runnerUrl: string | null = null;
     internalWorker: Worker | null = null;
     promise: Promise<T>;
     constructor(executor: PromiseExecutor<T>) {
-        let resolve: PromiseResolve<T>;
-        let reject: PromiseReject;
-        this.promise = new Promise((_resolve, _reject) => {
-            resolve = _resolve;
-            reject = _reject;
+        let resolver: PromiseResolve<T>;
+        let rejector: PromiseReject;
+        this.promise = new Promise((resolve, reject) => {
+            resolver = resolve;
+            rejector = reject;
         });
-        this.runnerCode = createIIFE(WorkerPromiseController.runnerTemplate).replaceAll("$func", executor.toString());
+        this.runnerCode = createIIFE(AsyncWorker.runnerTemplate).replaceAll("$func", executor.toString());
         this.runnerUrl = URL.createObjectURL(new Blob([this.runnerCode], { type: "text/javascript" }));
         this.internalWorker = new Worker(this.runnerUrl);
         this.internalWorker.addEventListener("message", (event) => {
             const { status, data, error } = event.data;
             if (status === "resolve") {
                 this.reset();
-                resolve(data);
+                resolver(data);
             } else if (status === "reject") {
                 this.reset();
-                reject(error);
+                rejector(error);
             }
         });
     }
@@ -44,6 +44,6 @@ export class WorkerPromiseController<T> {
     };
 }
 export function promiser<T>(executor: PromiseExecutor<T>) {
-    const controller = new WorkerPromiseController(executor);
+    const controller = new AsyncWorker(executor);
     return Object.assign(controller.promise, { controller });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { promiser } from "@promiser/index";
 console.log("start");
-await promiser<void>((resolve) => {
-    for (let i = 0; i < 10000000000; i++) { }
-    resolve();
+requestAnimationFrame(() => {
+    new Promise<void>((resolve) => {
+        for (let i = 0; i < 9999999999; i++) { }
+        resolve();
+    })
+        // promiser<void>((resolve) => {
+        //     for (let i = 0; i < 9999999999; i++) { }
+        //     resolve();
+        // })
+        .then(() => {
+            console.log("end");
+        });
 });
-console.log("end");
